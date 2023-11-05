147694d10b40a6d1d5b89f6f63ab46b3a04360b0
MrHumidum <salavatcool5@gmail.com>
2023-11-05 15:42:34 +0300

    none

--- cmd/database.py
+++ cmd/database.py
@@ -4,26 +4,52 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-a = ('Токио', 20, 'облачно')
 
-try:
-    conn = psycopg2.connect(host=os.getenv("host"),
-                            user=os.getenv("user"),
-                            password=os.getenv("password"),
-                            database=os.getenv("dbname"),
-                            port=os.getenv("port")
-                            )
-
-    conn.autocommit = True
-
-    # cursor
-    with conn.cursor() as cur:
-        cur.execute(
-            "INSERT INTO weather (city_name, temp, conditions) VALUES (%s, %s, %s)", a
+def connect_to_database():
+    try:
+        conn = psycopg2.connect(
+            host=os.getenv("host"),
+            user=os.getenv("user"),
+            password=os.getenv("password"),
+            database=os.getenv("dbname"),
+            port=os.getenv("port")
         )
+        conn.autocommit = True
+        return conn
+    except Exception as e:
+        print('Error:', e)
+        return None
 
-except Exception as e:
-    print('Error', e)
-finally:
+
+def close_database_connection(conn):
     if conn:
         conn.close()
+
+
+def get_weather():
+    conn = connect_to_database()
+    if conn:
+        try:
+            with conn.cursor() as cur:
+                cur.execute("SELECT * FROM weather")
+                return cur.fetchall()
+        except Exception as e:
+            print('Error:', e)
+        finally:
+            close_database_connection(conn)
+
+    return []
+
+
+def insert_weather(a):
+    conn = connect_to_database()
+    if conn:
+        try:
+            with conn.cursor() as cur:
+                cur.execute(
+                    "INSERT INTO weather (city_name, temp, conditions) VALUES (%s, %s, %s)", a
+                )
+        except Exception as e:
+            print('Error:', e)
+        finally:
+            close_database_connection(conn)
--- cmd/main.py
+++ cmd/main.py
@@ -1,9 +1,10 @@
 from PyQt5 import uic
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QApplication, QMainWindow, QTableView
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableView, QStandardItemModel, QStandardItem
 import io, os, sys, requests
 from dotenv import load_dotenv
 import psycopg2
+from database import connect_to_database, get_weather, insert_weather
 
 load_dotenv()
 
@@ -29,17 +30,53 @@ def openweathermap(city_name):
         pass
 
 
+# class Pogoda(QMainWindow):
+#     def __init__(self):
+#         super().__init__()
+#         uic.loadUi('template.ui', self)
+#         f = io.StringIO('template.ui')
+#         self.pushButton.clicked.connect(self.run)
+#         self.tableView.setModel(QStandardItemModel())
+#         self.con = connect_to_database
+#
+#
+#     def run(self):
+#         info = (openweathermap(self.poisk.displayText()))
+#         # self.history_city.append(self.poisk.text())
+#         self.label.setText(f'{info}')
+
 class Pogoda(QMainWindow):
     def __init__(self):
-        super().__init__()
+        super().__init()
         uic.loadUi('template.ui', self)
-        f = io.StringIO('template.ui')
         self.pushButton.clicked.connect(self.run)
+        self.table_view = self.findChild(QTableView, 'tableView')  # Находит QTableView по имени
+
+        # Создаем модель данных для QTableView
+        self.model = QStandardItemModel(self)
 
     def run(self):
-        info = (openweathermap(self.poisk.displayText()))
-        # self.history_city.append(self.poisk.text())
-        self.label.setText(f'{info}')
+        city_name = self.poisk.displayText()
+        weather_info = openweathermap(city_name)
+        self.label.setText(f'Погода: {weather_info["Погода"]}, Температура: {weather_info["Температура"]}')
+        insert_weather((city_name, weather_info["Температура"], weather_info["Погода"]))
+
+        # Загрузите данные из базы данных
+        data = get_weather()
+
+        # Очистите модель данных
+        self.model.clear()
+
+        # Устанавливаем заголовки для столбцов
+        self.model.setHorizontalHeaderLabels(["Город", "Температура", "Погода"])
+
+        # Заполните модель данными из базы данных
+        for row in data:
+            items = [QStandardItem(str(item)) for item in row]
+            self.model.appendRow(items)
+
+        # Устанавливаем модель данных для QTableView
+        self.table_view.setModel(self.model)
 
 
 # a = {'icoid': data['weather'][0]['icon'],
