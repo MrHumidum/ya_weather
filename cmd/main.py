64816113bfa73af6b654c8d542a5ca911ad4af82
MrHumidum <salavatcool5@gmail.com>
2023-11-06 19:30:35 +0300

    uncorrect city name error correction

--- cmd/main.py
+++ cmd/main.py
@@ -36,25 +36,30 @@ class Pogoda(QMainWindow):
     def run(self):
         city_name = self.poisk.displayText()
         weather_info = openweathermap(city_name)
-        self.label.setText(f'Погода: {weather_info["Погода"]}')
-        self.label_2.setText(f'Температура: {weather_info["Температура"]}°C')
+        if weather_info == None:
+            print('[ERROR] Некорректное название города')
+            self.label.setText(f'[ERROR] Некорректное название города')
+            self.label_2.setText(f'[ERROR] Некорректное название города')
+        else:
+            self.label.setText(f'Погода: {weather_info["Погода"]}')
+            self.label_2.setText(f'Температура: {weather_info["Температура"]}°C')
 
-        pixmap = QPixmap(f"ico/{weather_info['icoid']}@2x.png")
-        self.label_picture.setPixmap(pixmap)
-        self.label_picture.setScaledContents(True)
-        insert_weather((city_name, weather_info["Температура"], weather_info["Погода"]))
+            pixmap = QPixmap(f"ico/{weather_info['icoid']}@2x.png")
+            self.label_picture.setPixmap(pixmap)
+            self.label_picture.setScaledContents(True)
+            insert_weather((city_name, weather_info["Температура"], weather_info["Погода"]))
 
-        data = get_weather()
+            data = get_weather()
 
-        self.model.clear()
+            self.model.clear()
 
-        self.model.setHorizontalHeaderLabels(["Дата и время", "Город", "Температура", "Погода"])
+            self.model.setHorizontalHeaderLabels(["Дата и время", "Город", "Температура", "Погода"])
 
-        for row in data:
-            items = [QStandardItem(str(item)) for item in row]
-            self.model.appendRow(items)
+            for row in data:
+                items = [QStandardItem(str(item)) for item in row]
+                self.model.appendRow(items)
 
-        self.table_view.setModel(self.model)
+            self.table_view.setModel(self.model)
 
 
 if __name__ == '__main__':
